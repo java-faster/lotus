adb6378c256583493b11cb011c444be739ff15fa
lotus <1126164600@qq.com>
2019-04-29 17:57:24 +0800

    用户工程

---
+++ mamabuy-user-server/src/main/java/com/coder520/mamabuyuserserver/MamabuyUserServerApplication.java
@@ -0,0 +1,13 @@
+package com.coder520.mamabuyuserserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MamabuyUserServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MamabuyUserServerApplication.class, args);
+    }
+
+}
