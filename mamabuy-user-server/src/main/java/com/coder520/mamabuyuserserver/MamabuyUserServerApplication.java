e0dc107fceb9b6cf6a2633b6f4b900c13a52dffa
lotus <1126164600@qq.com>
2020-01-30 08:59:30 +0800

    全部提交

--- mamabuy-user-server/src/main/java/com/coder520/mamabuyuserserver/MamabuyUserServerApplication.java
+++ mamabuy-user-server/src/main/java/com/coder520/mamabuyuserserver/MamabuyUserServerApplication.java
@@ -1,13 +1,44 @@
 package com.coder520.mamabuyuserserver;
 
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class MamabuyUserServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MamabuyUserServerApplication.class, args);
+    }
+
+    /**
+     * FastJsonHttpMessageConverter
+     * @return
+     */
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverters() {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        HttpMessageConverter<?> converter = fastConverter;
+        return new HttpMessageConverters(converter);
+    }
+
+    /**
+     * 用于properties文件占位符解析
+     * @return
+     */
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
